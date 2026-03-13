678b141c988f6669bb47c5e97010e979f11f7783
Ricardo Maldonado <rimalca98@gmail.com>
2026-03-12 22:37:55 -0500

    Se modifico codigo

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 while True:
+    nombre = input("Nombre de producto: ")
+    if nombre.isalpha():
+        break
+    print("Solo se permiten letras, intenta de nuevo.")
+
+while True:
     try:
-        nombre = input("Ingrese nombre: ")
-        if not nombre or not nombre.isalpha():
-            if not nombre:
-                print("Error: El nombre no puede estar vacío.")
-            elif not nombre.isalpha():
-                print("Error: El nombre debe contener solo letras.")
+        precio = float(input("Precio: "))
+        if precio > 0:
+            break
+        print("Debe ser mayor que 0...")
     except ValueError:
-        print("Error: Por favor ingrese un nombre válido.")
-
-    try:
-        precio = float(input("Ingrese precio: "))
-        if precio < 0:
-            print("Error: El precio no puede ser negativo.")
-    except ValueError:
-        print("Error: Por favor ingrese un precio válido.")
-        continue
-
-    try:    
-        cantidad = int(input("Ingrese cantidad: "))
-        if cantidad < 0:
-            print("Error: La cantidad no puede ser negativa.")
-            continue
-    except ValueError:
-        print("Error: Por favor ingrese valores válidos.")
-
+         print("Ingrese un valor valido...")
